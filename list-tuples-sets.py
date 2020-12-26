bcf25d255996b9f4da8b02111b8263259ea27406
zunayed <zunayed.arp@gmail.com>
2020-12-26 22:49:25 +0600

    integers and float

--- list-tuples-sets.py
+++ list-tuples-sets.py
@@ -1,21 +1,10 @@
 courses = ['History','Math','Physics','CompSci']
-courses_2 = ['Computer','Education']
-print(courses[:])
 
-courses.append("Art")
+courses.insert(0,"Chemistry")
+print(courses[2:])
 print(courses)
-courses.insert(0,'Art')
-print(courses)
-print(courses)
-courses.extend(courses_2)
-print(courses)
-courses.remove("History")
-print(courses)
-popped = courses.pop()
 
-print(popped)
-nums = [1, 5, 2, 4, 3]
-sorted_courses = sorted(courses)
-print(courses)
-print(help(courses))
+print(courses.index('CompSci'))
 
+for index,course in enumerate(courses):
+    print(index, course)
