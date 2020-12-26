67d786ec5e2df9623d5925b08286585febc59edb
zunayed <zunayed.arp@gmail.com>
2020-12-26 21:25:19 +0600

    integers and float

--- list-tuples-sets.py
+++ list-tuples-sets.py
@@ -9,3 +9,13 @@ print(courses)
 print(courses)
 courses.extend(courses_2)
 print(courses)
+courses.remove("History")
+print(courses)
+popped = courses.pop()
+
+print(popped)
+nums = [1, 5, 2, 4, 3]
+sorted_courses = sorted(courses)
+print(courses)
+print(help(courses))
+
